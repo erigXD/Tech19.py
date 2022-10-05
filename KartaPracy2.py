6ffb783ff12ebc88990402adf078e28636f7247e
18EM <114591635+erigXD@users.noreply.github.com>
2022-10-05 05:57:28 +0000

    Zadania Zrobione(7 zadań)

---
+++ KartaPracy2.py
@@ -0,0 +1,62 @@
+# Zad 1
+
+# a = int(input())
+# if a % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 2
+
+# a = int(input())
+# if a >99 and a < 1000:
+#   if a % 17 == 0:
+#     print("TAK")
+#   else:
+#     print("NIE")
+# else:
+#   print("Nie jest trzycyfrowa.")
+
+# Zad 3
+
+# wiek = int(input())
+# if wiek >= 18:
+#   print("Osoba pełnoletnia")
+# else:
+#   print("Osoba niepełnoletnia")
+
+# Zad 4
+
+# limit = 20
+#waga = int(input("waga: "))
+# if waga > limit:
+#   print("NIE")
+# else:
+#   print("TAK")
+
+# Zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# a, b, c = map(int, input().split()) # trzy inputy na raz
+# if (c < b and c > a) or (b < c and c < a):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 6
+
+# p = int(input("podaj liczbę pierwszą: "))
+# a = int(input("podaj dowolną liczbę: "))
+# if (a**p - a) % p == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 7
+
+# p, k, s = map(int, input().split())
+# if (k - p) <= 3*s or (3*s + p) >= k:
+#   print("TAK")
+# else:
+#   print("NIE")
