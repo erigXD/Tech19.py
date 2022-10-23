4f05ab108f4d59b989192861d82aaa56c4fec52d
18EM <114591635+erigXD@users.noreply.github.com>
2022-10-23 16:57:39 +0000

    Ukończono KartaPracy3b.py

---
+++ KartaPracy3b.py
@@ -0,0 +1,66 @@
+# Zad 1 
+# for i in range(1,31):
+#   print(i)
+
+# Zad 2
+# n = int(input())
+# for i in range(1,n+1,2):
+#   print(i**2)
+
+# Zad 3
+# for i in range(1000,10000,379):
+#   print(i)
+
+# Zad 4
+# for i in range(100,1000):
+#   if i % 5 == 0 or i % 6 == 0 or i % 11 == 0:
+#     print(i)
+
+# Zad 5
+# nliczb = int(input("Ile liczb: "))
+# Liczby = []
+# for i in range(1,nliczb+1):
+#   n = int(input())
+#   Liczby.append(n)
+# print(sum(Liczby))
+
+# Zad 6
+# k = int(input())
+# suma = 0
+# for i in range(2,(k*2)+2,2):
+#   suma += i
+# print(suma)
+
+# Zad 7
+# m = int(input())
+# suma = 0
+# for i in range(11,(m*2)+11,2):
+#   suma += i
+# print(suma)
+
+# Zad 8
+# W0 = int(input())
+# L = int(input())
+# L = L * 12
+# W1 = W0
+# for i in range(0,L):
+#   W0 = W0 + (W1 * 0.005)
+#   print(W0)
+
+# Zad 9
+# n = int(input())
+# a = 21
+# suma = 0
+# for i in range(0,n+1):
+#   for i1 in range(0,i,a):
+#     suma += a
+#     a += 100
+# print(suma)
+
+# Zad 10
+# from math import sqrt
+# for i in range(1,1000):
+#   if i - (i//10)*10 == sqrt(i):
+#     print(i)
+#   elif i - (i//100)*100 == sqrt(i):
+#     print(i)
